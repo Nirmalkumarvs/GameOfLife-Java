5553097717f5d9f4d362996c28f119e61acf93f0
Nirmal <nirmalkumarvs82@gmail.com>
2022-05-29 10:17:58 +0530

    Ensured DIRECTIONS attribute is Accessible

--- lib/src/main/java/GameOfLife/GameOfLife.java
+++ lib/src/main/java/GameOfLife/GameOfLife.java
@@ -1,5 +1,5 @@
 package GameOfLife;
 
 public class GameOfLife {
-
+    public int DIRECTIONS[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 }
--- lib/src/test/java/GameOfLife/GameOfLifeTest.java
+++ lib/src/test/java/GameOfLife/GameOfLifeTest.java
@@ -3,9 +3,17 @@ package GameOfLife;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class GameOfLifeTest {
     @Test
     public void testGameOfLifeClassCanBeInitialized() {
         GameOfLife gameOfLife = new GameOfLife();
+    }
+
+    @Test
+    public void testDirectionsAttributeIsAvailable() {
+        GameOfLife gameOfLife = new GameOfLife();
+        assertNotNull(gameOfLife.DIRECTIONS);
     }
 }
